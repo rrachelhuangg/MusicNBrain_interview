df2484fb15d50980301b02e2d74c57ffe62d6acd
Rachel Huang <rachelhuang@Rachels-Laptop.local>
2024-11-11 00:39:25 -0500

    Implemented basic spotify audio analysis of tempo and key

--- five_second_music_extraction.py
+++ five_second_music_extraction.py
@@ -11,3 +11,23 @@
 
 # harmonic analysis/time series analysis?
 
+import librosa
+import spotipy
+import numpy as np
+from spotipy.oauth2 import SpotifyClientCredentials
+
+client_id = "1b03b661f34d492eaabc7c330c4fe7b0"
+client_secret = "db06c4a9312a46eda1bbf8a79aa8f53e"
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def analyze_song(track_id):
+    features = spotify.audio_features(track_id)
+    tempo = features[0]['tempo'] if features else None
+    key = features[0]['key'] if features else None
+
+    return {"tempo": tempo, "key": key}
+
+track_id = "2iUmqdfGZcHIhS3b9E9EWq"
+sections = analyze_song(track_id)
+print(sections)
