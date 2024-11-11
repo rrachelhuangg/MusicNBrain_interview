d82b8dd15741751c673395c78b7204087dbdcec6
Rachel Huang <rachelhuang@Rachels-Laptop.local>
2024-11-11 00:25:18 -0500

    Hashing out ideas

---
+++ five_second_music_extraction.py
@@ -0,0 +1,13 @@
+#show good documentation and clean code, examples
+
+#very broad prompt: locate the most interesting 5 seconds from a song
+
+#types of song input: audio files (from spotify, soundcloud), text files of the notes (assip), song title
+#definition of interesting 5 seconds: main melody, loudest, fastest, most instruments used, variation on the melody
+
+# idea 1: use librosa and spotipy to select fastest/loudest/highest energy... parts of songs using track ids
+
+# idea 2: assip method: musescore plugin to convert sheet music to abc (export notes?) text file: and then do analysis with note patterns on that (and possibly try regex?)
+
+# harmonic analysis/time series analysis?
+
